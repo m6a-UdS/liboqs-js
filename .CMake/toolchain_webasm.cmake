075d294fb40e372ac29f0daa77bc0bbfce2fa08c
Max White <mleblanc@poka.io>
2020-07-12 14:29:45 -0400

    WIP

---
+++ .CMake/toolchain_webasm.cmake
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: MIT
+
+# How to use:
+# apt install gcc-8-arm-linux-gnueabihf
+# cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=../.CMake/toolchain_rasppi.cmake -DOQS_USE_OPENSSL=OFF ..
+
+set(CMAKE_CROSSCOMPILING ON)
+
+set(CMAKE_C_COMPILER emcc)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
